6da5ed8e4c037e7a3436816630e68f21aac8b372
philgamer999 <phili@LORDFUNSHY>
2024-02-27 20:59:57 +0100

    Logic Added

--- PCRunTime/Form1.Designer.cs
+++ PCRunTime/Form1.Designer.cs
@@ -34,120 +34,135 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.LabelTimeTotal = new System.Windows.Forms.Label();
+            this.LabelTimeToday = new System.Windows.Forms.Label();
+            this.LabelTimeCurrent = new System.Windows.Forms.Label();
+            this.ButtonSaveQuit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LabelTitle
             // 
             this.LabelTitle.AutoSize = true;
-            this.LabelTitle.Font = new System.Drawing.Font("Minecraftia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelTitle.ForeColor = System.Drawing.Color.Lime;
             this.LabelTitle.Location = new System.Drawing.Point(125, 31);
             this.LabelTitle.Name = "LabelTitle";
-            this.LabelTitle.Size = new System.Drawing.Size(141, 24);
+            this.LabelTitle.Size = new System.Drawing.Size(113, 20);
             this.LabelTitle.TabIndex = 0;
             this.LabelTitle.Text = "PC Run Time";
             // 
             // LabelCompany
             // 
             this.LabelCompany.AutoSize = true;
-            this.LabelCompany.Font = new System.Drawing.Font("Minecraftia", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelCompany.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelCompany.ForeColor = System.Drawing.Color.Lime;
             this.LabelCompany.Location = new System.Drawing.Point(0, 249);
             this.LabelCompany.Name = "LabelCompany";
-            this.LabelCompany.Size = new System.Drawing.Size(29, 12);
+            this.LabelCompany.Size = new System.Drawing.Size(24, 9);
             this.LabelCompany.TabIndex = 1;
             this.LabelCompany.Text = "comp";
             // 
             // LabelVersion
             // 
             this.LabelVersion.AutoSize = true;
-            this.LabelVersion.Font = new System.Drawing.Font("Minecraftia", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelVersion.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelVersion.ForeColor = System.Drawing.Color.Lime;
             this.LabelVersion.Location = new System.Drawing.Point(0, 237);
             this.LabelVersion.Name = "LabelVersion";
             this.LabelVersion.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.LabelVersion.Size = new System.Drawing.Size(13, 12);
+            this.LabelVersion.Size = new System.Drawing.Size(11, 9);
             this.LabelVersion.TabIndex = 2;
             this.LabelVersion.Text = "v.";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Lime;
             this.label1.Location = new System.Drawing.Point(73, 87);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Current Time:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Lime;
             this.label2.Location = new System.Drawing.Point(73, 137);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.Size = new System.Drawing.Size(66, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Today Time:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Lime;
             this.label3.Location = new System.Drawing.Point(73, 187);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.Size = new System.Drawing.Size(60, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Total Time:";
             // 
-            // label4
+            // LabelTimeTotal
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Lime;
-            this.label4.Location = new System.Drawing.Point(214, 187);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 17);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "00 : 00 : 00";
+            this.LabelTimeTotal.AutoSize = true;
+            this.LabelTimeTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTimeTotal.ForeColor = System.Drawing.Color.Lime;
+            this.LabelTimeTotal.Location = new System.Drawing.Point(214, 187);
+            this.LabelTimeTotal.Name = "LabelTimeTotal";
+            this.LabelTimeTotal.Size = new System.Drawing.Size(61, 13);
+            this.LabelTimeTotal.TabIndex = 8;
+            this.LabelTimeTotal.Text = "00 : 00 : 00";
             // 
-            // label5
+            // LabelTimeToday
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Lime;
-            this.label5.Location = new System.Drawing.Point(214, 137);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(90, 17);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "00 : 00 : 00";
+            this.LabelTimeToday.AutoSize = true;
+            this.LabelTimeToday.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTimeToday.ForeColor = System.Drawing.Color.Lime;
+            this.LabelTimeToday.Location = new System.Drawing.Point(214, 137);
+            this.LabelTimeToday.Name = "LabelTimeToday";
+            this.LabelTimeToday.Size = new System.Drawing.Size(61, 13);
+            this.LabelTimeToday.TabIndex = 7;
+            this.LabelTimeToday.Text = "00 : 00 : 00";
             // 
-            // label6
+            // LabelTimeCurrent
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Lime;
-            this.label6.Location = new System.Drawing.Point(214, 87);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(90, 17);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "00 : 00 : 00";
+            this.LabelTimeCurrent.AutoSize = true;
+            this.LabelTimeCurrent.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTimeCurrent.ForeColor = System.Drawing.Color.Lime;
+            this.LabelTimeCurrent.Location = new System.Drawing.Point(214, 87);
+            this.LabelTimeCurrent.Name = "LabelTimeCurrent";
+            this.LabelTimeCurrent.Size = new System.Drawing.Size(61, 13);
+            this.LabelTimeCurrent.TabIndex = 6;
+            this.LabelTimeCurrent.Text = "00 : 00 : 00";
+            // 
+            // ButtonSaveQuit
+            // 
+            this.ButtonSaveQuit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtonSaveQuit.ForeColor = System.Drawing.Color.Lime;
+            this.ButtonSaveQuit.Location = new System.Drawing.Point(120, 215);
+            this.ButtonSaveQuit.Margin = new System.Windows.Forms.Padding(2);
+            this.ButtonSaveQuit.Name = "ButtonSaveQuit";
+            this.ButtonSaveQuit.Size = new System.Drawing.Size(100, 20);
+            this.ButtonSaveQuit.TabIndex = 9;
+            this.ButtonSaveQuit.Text = "Save and Quit";
+            this.ButtonSaveQuit.UseVisualStyleBackColor = true;
+            this.ButtonSaveQuit.Click += new System.EventHandler(this.ButtonSaveQuit_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(384, 261);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label6);
+            this.ClientSize = new System.Drawing.Size(383, 259);
+            this.Controls.Add(this.ButtonSaveQuit);
+            this.Controls.Add(this.LabelTimeTotal);
+            this.Controls.Add(this.LabelTimeToday);
+            this.Controls.Add(this.LabelTimeCurrent);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -155,8 +170,8 @@
             this.Controls.Add(this.LabelCompany);
             this.Controls.Add(this.LabelTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximumSize = new System.Drawing.Size(400, 300);
-            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.MaximumSize = new System.Drawing.Size(399, 298);
+            this.MinimumSize = new System.Drawing.Size(399, 298);
             this.Name = "MainWindow";
             this.Text = "PCRunTime";
             this.ResumeLayout(false);
@@ -172,9 +187,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label LabelTimeTotal;
+        private System.Windows.Forms.Label LabelTimeToday;
+        private System.Windows.Forms.Label LabelTimeCurrent;
+        private System.Windows.Forms.Button ButtonSaveQuit;
     }
 }
 
--- PCRunTime/Form1.cs
+++ PCRunTime/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PCRunTime
@@ -18,6 +12,12 @@ namespace PCRunTime
         public string dailyFilePath = string.Empty;
         public string date = DateTime.Now.ToString("yyyy_MM_dd");
 
+        public int cHour, cMinute, cSecond;
+        public int dHour, dMinute, dSecond;
+        public int tHour, tMinute, tSecond;
+
+        public Timer timer = new Timer();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,16 +27,78 @@ namespace PCRunTime
 
             SetPaths();
             CreateFiles();
+            GetData();
+
+            timer.Interval = 1000;
+            timer.Start();
+            timer.Tick += new EventHandler(TimerTick);
         }
 
 
+        public void TimerTick(object sender, EventArgs e)
+        {
+            if (cSecond == 59)
+            {
+                cSecond = 0;
+                cMinute++;
+                SaveData();
+            }
+            else
+            {
+                cSecond++;
+            }
+
+            if (cMinute == 59)
+            {
+                cMinute = 0;
+                cHour++;
+            }
+
+            if (dSecond == 59)
+            {
+                dSecond = 0;
+                dMinute++;
+                SaveData();
+            }
+            else
+            {
+                dSecond++;
+            }
+
+            if (dMinute == 59)
+            {
+                dMinute = 0;
+                dHour++;
+            }
+
+
+            if (tSecond == 59)
+            {
+                tSecond = 0;
+                tMinute++;
+                SaveData();
+            }
+            else
+            {
+                tSecond++;
+            }
+
+            if (tMinute == 59)
+            {
+                tMinute = 0;
+                tHour++;
+            }
+
+            LabelTimeCurrent.Text = $"{cHour.ToString("D2")} : {cMinute.ToString("D2")} : {cSecond.ToString("D2")}";
+            LabelTimeToday.Text = $"{dHour.ToString("D2")} : {dMinute.ToString("D2")} : {dSecond.ToString("D2")}";
+            LabelTimeTotal.Text = $"{tHour.ToString("D2")} : {tMinute.ToString("D2")} : {tSecond.ToString("D2")}";
+        }
 
         public void SetPaths()
         {
             dataPath = Application.StartupPath + "/data";
             totalFilePath = dataPath + "/total.txt";
             dailyFilePath = dataPath + "/daily_" + date + ".txt";
-            MessageBox.Show(dailyFilePath);
         }
 
         public void CreateFiles()
@@ -55,6 +117,38 @@ namespace PCRunTime
             {
                 File.Create(dailyFilePath);
             }
+        }
+
+        public void GetData()
+        {
+            char[] dailyChars = File.ReadAllText(dailyFilePath).ToCharArray();
+            char[] totalChars = File.ReadAllText(dailyFilePath).ToCharArray();
+
+            dSecond = int.Parse(string.Join("", dailyChars.Skip(0).Take(2)));
+            dMinute = int.Parse(string.Join("", dailyChars.Skip(3).Take(2)));
+            dHour = int.Parse(string.Join("", dailyChars.Skip(6)));
+
+            tSecond = int.Parse(string.Join("", totalChars.Skip(0).Take(2)));
+            tMinute = int.Parse(string.Join("", totalChars.Skip(3).Take(2)));
+            tHour = int.Parse(string.Join("", totalChars.Skip(6)));
+        }
+
+        public void SaveData()
+        {
+            File.WriteAllText(totalFilePath, $"{tSecond.ToString("D2")}:{tMinute.ToString("D2")}:{tHour.ToString("D2")}");
+            File.WriteAllText(dailyFilePath, $"{dSecond.ToString("D2")}:{dMinute.ToString("D2")}:{dHour.ToString("D2")}");
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            SaveData();
+        }
+
+        private void ButtonSaveQuit_Click(object sender, EventArgs e)
+        {
+            SaveData();
+            Application.Exit();
         }
     }
 }

ef84e3003fe3569f50904446c6902af0d551f46e
philgamer999 <philip.werner.06@gmail.com>
2024-03-03 23:24:06 +0100

    Repos

--- PCRunTime/Form1.Designer.cs
+++ PCRunTime/Form1.Designer.cs
@@ -80,7 +80,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Lime;
-            this.label1.Location = new System.Drawing.Point(49, 51);
+            this.label1.Location = new System.Drawing.Point(49, 68);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 17);
             this.label1.TabIndex = 3;
@@ -104,7 +104,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Lime;
-            this.label3.Location = new System.Drawing.Point(49, 151);
+            this.label3.Location = new System.Drawing.Point(49, 135);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(79, 17);
             this.label3.TabIndex = 5;
@@ -116,7 +116,7 @@
             this.LabelTimeTotal.AutoSize = true;
             this.LabelTimeTotal.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelTimeTotal.ForeColor = System.Drawing.Color.Lime;
-            this.LabelTimeTotal.Location = new System.Drawing.Point(190, 151);
+            this.LabelTimeTotal.Location = new System.Drawing.Point(190, 135);
             this.LabelTimeTotal.Name = "LabelTimeTotal";
             this.LabelTimeTotal.Size = new System.Drawing.Size(90, 17);
             this.LabelTimeTotal.TabIndex = 8;
@@ -140,7 +140,7 @@
             this.LabelTimeCurrent.AutoSize = true;
             this.LabelTimeCurrent.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LabelTimeCurrent.ForeColor = System.Drawing.Color.Lime;
-            this.LabelTimeCurrent.Location = new System.Drawing.Point(190, 51);
+            this.LabelTimeCurrent.Location = new System.Drawing.Point(190, 68);
             this.LabelTimeCurrent.Name = "LabelTimeCurrent";
             this.LabelTimeCurrent.Size = new System.Drawing.Size(90, 17);
             this.LabelTimeCurrent.TabIndex = 6;
@@ -152,7 +152,7 @@
             this.ButtonSaveQuit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.ButtonSaveQuit.Font = new System.Drawing.Font("Minecraftia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ButtonSaveQuit.ForeColor = System.Drawing.Color.Lime;
-            this.ButtonSaveQuit.Location = new System.Drawing.Point(108, 176);
+            this.ButtonSaveQuit.Location = new System.Drawing.Point(108, 170);
             this.ButtonSaveQuit.Margin = new System.Windows.Forms.Padding(2);
             this.ButtonSaveQuit.Name = "ButtonSaveQuit";
             this.ButtonSaveQuit.Size = new System.Drawing.Size(117, 31);
